3646e2733562c48666a76240f19698cf0c29f9e9
Vincent Han <vhan@bowdoin.edu>
2024-04-24 19:29:13 -0400

    4/24

--- Agents/BayesianAgent.py
+++ Agents/BayesianAgent.py
@@ -1,34 +1,40 @@
 import random as rand
+from numpy import random
 
 class BayesianAgent:
     
-    def __init__(self, id, cash):
+    def __init__(self, id, variance, lam):
         self.agent_id = id
-        self.holdings = 0
-        self.cash = cash
         self.holdingLimit = 1000
-        self.variance = rand.randint(5, 15)
+        self.variance = variance
+        self.timer = 0
+        self.lam = lam
     
     def take_action(self, midpoint):
         belief = rand.normalvariate(midpoint, self.variance)
-        if belief > midpoint:
-            k = (abs(belief - midpoint)) / self.variance
+        if self.timer == 0:
+            if belief > midpoint:
+                k = (abs(belief - midpoint)) / self.variance
 
-            if k < 1:
-                amount = k * self.holdingLimit
-            else:
-                amount = self.holdingLimit
-            
-            return 1, belief, amount
-        elif belief == midpoint:
-            return None
-        else:
-            k = (abs(belief - midpoint)) / self.variance
-
-            if k < 1:
-                amount = k * self.holdingLimit
-            else:
-                amount = self.holdingLimit
+                if k < 1:
+                    amount = k * self.holdingLimit
+                else:
+                    amount = self.holdingLimit
+                self.timer = random.poisson(self.lam, 1)
                 
-            return 0, belief, amount
+                return 1, belief, amount
+            elif belief == midpoint:
+                return None
+            else:
+                k = (abs(belief - midpoint)) / self.variance
+
+                if k < 1:
+                    amount = k * self.holdingLimit
+                else:
+                    amount = self.holdingLimit
+                self.timer = random.poisson(self.lam, 1)
+                return 0, belief, amount
+        else:
+            self.timer -= 1
+            return None
     
--- Agents/RandomAgent.py
+++ Agents/RandomAgent.py
@@ -1,12 +1,13 @@
 import random as rand
+from numpy import random
 
 class RandomAgent:
     
-    def __init__(self, id, cash):
-        self.agent_id = id
-        self.holdings = 0
-        self.cash = cash
-        self.holdingLimit = 1000
+    def __init__(self, agent_id, lam):
+        self.agent_id = agent_id
+        self.timer = 0
+        self.holdingLimit = 10000
+        self.lam = lam
     
     # buyPrices are prices at which buyers are willing to buy, which is used when selling
     # sellPrices are prices at which sellers are willing to sell, which is used when buying
@@ -14,24 +15,27 @@ class RandomAgent:
         
         # designate 0 to be sell, and 1 to be buy, 2 is do nothing
         decision = rand.randint(3)
-    
-        if decision == 0:
-            price = rand.choices(buyPrices)
-            shares = rand.randint(self.holdingLimit + 1)
-            
-            self.holdings -= shares    
-                
-            return decision, price, shares
+        if self.timer ==0:
+            if decision == 0:
+                price = rand.choices(buyPrices)
+                shares = rand.randint(self.holdingLimit + 1)
+
+                self.holdings -= shares    
+                self.timer = random.poisson(self.lam, 1)
+                return decision, price, shares
         
-        elif decision == 1:
-            price = rand.choices(sellPrices)
-            shares = rand.randint(self.holdingLimit + 1)
+            elif decision == 1:
+                price = rand.choices(sellPrices)
+                shares = rand.randint(self.holdingLimit + 1)
             
-            self.holdings += shares
-            
-            return decision, price, shares
+                self.holdings += shares
+                self.timer = random.poisson(self.lam, 1)
+                return decision, price, shares
         
+            else:
+                return None
         else:
-            
+            self.timer -= 1
             return None
+            
         
