6ac757c6eaf79fb2e05588ba80870061a9e990b7
Vincent Han <vhan@bowdoin.edu>
2024-04-23 21:22:21 -0400

    4/23

--- Experiments/RunTrades.py
+++ Experiments/RunTrades.py
@@ -10,7 +10,12 @@ class TradeWorld:
         self.randomAgents = []
         self.bayesianAgents = []
 
-        self.totalAgents = 100
+        self.totalAgents = 50
+        self.trips = 5
+        
+        self.fixed_cost = 0
+        self.floating_cost = 0
+        
         
         self.lob = LimitOrderBook() # need to write a function to read in a limit order book from stock data
 
@@ -32,11 +37,25 @@ class TradeWorld:
             else:
                 currAgent = self.bayesianAgents.pop()
                 
-            decision, price, shares = currAgent.take_action()
+            cashBeforeTrades = currAgent.cash
             
-            if decision == 0:
-                self.lob.fill_sell_order(price, shares, currAgent.agent_id)
+            values_buy = list(self.lob.buy_orders.values())
+            indices_buy = [key for key, val in self.lob.buy_orders.items() if val in values_buy]
+                
+            values_sell = list(self.lob.sell_orders.values())
+            indices_sell = [key for key, val in self.lob.sell_orders.items() if val in values_sell]
+            
+            for trip in range(1, self.trips):
+                
+                decision, price, shares = currAgent.take_action(indices_buy, indices_sell)
+                
+                if decision == 0:
+                    currAgent.cash += self.lob.fill_sell_order(price, shares, currAgent.agent_id) - self.fixed_cost - self.floating_cost * shares
+                else:
+                    currAgent.cash -= self.lob.fill_buy_order(price, shares, currAgent.agent_id) + self.fixed_cost + self.floating_cost * shares
+            
+            if type == 0:
+                print("This random agent made ", (currAgent.cash - cashBeforeTrades))
             else:
-                self.lob.fill_buy_order(price, shares, currAgent.agent_id)
+                print("This Bayesian agent made ", (currAgent.cash - cashBeforeTrades))
             
-            time.sleep(1)
